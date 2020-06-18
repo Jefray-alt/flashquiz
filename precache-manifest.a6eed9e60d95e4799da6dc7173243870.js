6e0adef3aef7cd123407f7c894b5711089783dc2
Jeffrey France <jbfrance1626@gmail.com>
2020-06-18 14:30:42 +0800

    Updates

---
+++ precache-manifest.a6eed9e60d95e4799da6dc7173243870.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca0e005507c9198591cc1834f396976a",
+    "url": "/flashquiz/index.html"
+  },
+  {
+    "revision": "a5197b5bdffe06580a36",
+    "url": "/flashquiz/static/css/main.d24cbd79.chunk.css"
+  },
+  {
+    "revision": "0e618a0573ba2e7f042b",
+    "url": "/flashquiz/static/js/2.167ca76e.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/flashquiz/static/js/2.167ca76e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5197b5bdffe06580a36",
+    "url": "/flashquiz/static/js/main.ee119c48.chunk.js"
+  },
+  {
+    "revision": "199e46cd302438102be8",
+    "url": "/flashquiz/static/js/runtime-main.e6ada979.js"
+  },
+  {
+    "revision": "03181d9dfc7f5fdfb9ea2a28d761244a",
+    "url": "/flashquiz/static/media/spinner.03181d9d.gif"
+  }
+]);
